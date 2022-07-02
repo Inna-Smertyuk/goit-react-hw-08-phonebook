7310acf992611650bed4266af38f837833b98452
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2022-07-02 19:52:10 +0300

    corection
    .

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
-import { useSelector } from "react-redux";
-import authSelectors from "../../redux/auth/auth-selector";
-import s from "./Navigation.module.css";
-import { NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/auth/auth-selector';
+import { NavLink } from 'react-router-dom';
+import s from './Navigation.module.css';
 
 function Navigation() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
     <div className={s.navMenu}>
       <NavLink
-        className={({ isActive }) => (isActive ? s.linkActive : s.link)}
         to="/"
+        className={({ isActive }) => (isActive ? s.linkActive : s.link)}
       >
         Home
       </NavLink>
 
       {isLoggedIn && (
         <NavLink
-          className={({ isActive }) => (isActive ? s.linkActive : s.link)}
           to="/contacts"
+          className={({ isActive }) => (isActive ? s.linkActive : s.link)}
         >
           Contacts
         </NavLink>
